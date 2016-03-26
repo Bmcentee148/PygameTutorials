04b2a9d231733b31fe80102a77496c529b2d6f89
Brian McEntee <brian.m.mcentee@gmail.com>
2016-03-26 00:20:40 -0400

    Finish 'simple_graphics.py' and 'ball_bounce.py'

---
+++ ball_bounce.py
@@ -0,0 +1,37 @@
+import sys
+import pygame
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+
+width = 640
+height = 480
+size = width, height
+
+speed = [4, 4]
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load('ball.bmp')
+ball_rect = ball.get_rect()
+
+while True :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            sys.exit(0)
+
+
+    ball_rect = ball_rect.move(speed)
+
+    if ball_rect.left < 0 or ball_rect.right > width :
+        speed[0] = -1 * speed[0]
+    if ball_rect.top < 0 or ball_rect.bottom > height :
+        speed[1] = -1 * speed[1]
+
+    screen.fill(BLACK)
+    screen.blit(ball, ball_rect)
+    pygame.display.flip()
+
+pygame.quit()
+
--- simple_graphics_demo.py
+++ simple_graphics_demo.py
@@ -10,6 +10,8 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
+PI = 3.141592653
+
 pygame.init()
 
 # Set width and height of the screen
@@ -50,10 +52,33 @@ while not done :
     # Draw an ellipse using a rectangle as the outside boundaries
     pygame.draw.ellipse(screen, RED, [20,20,250,100], 2)
 
-    # ----- Update Screen With What Was Drawn
+    # Draw an ellipse via 4 arcs. Use radians to determine which part.
+    pygame.draw.arc(screen, BLACK, [20,220,250,200], 0, PI / 2, 2)
+    pygame.draw.arc(screen, RED, [20,220,250,200], PI / 2, PI , 2)
+    pygame.draw.arc(screen, BLUE, [20,220,250,200], PI, 3 * PI / 2, 2)
+    pygame.draw.arc(screen, GREEN, [20,220,250,200], 3 * PI / 2, 2 * PI, 2)
+
+    # Draw a triangle using the polygon function
+    pygame.draw.polygon(screen, BLACK, [[350,250],[600,250],[475,150]], 3)
+
+    pygame.draw.polygon(screen, BLACK, [[100,100],[0,200],[200,200]], 3)
+
+    # Select font to use, size, bold, italics
+    font = pygame.font.SysFont('Calibiri',25,True,False)
+
+    # Render the text. "True" means anti-aliased text.
+    # Black is the color. This creates an image of the
+    # letters, but does not put it on the screen
+    text = font.render("My text", True, BLACK)
+
+    # Put the image of the text on the screen at 250x250
+    screen.blit(text, [250,250])
+
+    # Update Screen With What Was Drawn
+    # This MUST happen after all of the drawing commands
     pygame.display.flip()
 
-    # ----- Limit to 60 frames a second
+    # Limit to 60 frames a second, otherwise we will all of cpu we can
     clock.tick(60)
 
 # Close the window and quit
